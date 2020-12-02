a0238d61d0e2dddc8d6b48ef314b1c0bb932c841
PENG <thuwangp@gmail.com>
2020-12-01 23:08:20 -0500

    add data preprocessing

---
+++ utils/select_videos.py
@@ -0,0 +1,21 @@
+import json
+import argparse
+import pickle as pkl
+from collections import defaultdict
+
+
+def split_train_valid():
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    params = vars(args)  # convert to ordinary dict
+
+    main(params)
